ce3a389e415e5778487da097a947600234bd45ba
Destof <webdestof@gmail.com>
2019-08-22 04:34:02 +0500

    2 blocks

---
+++ Build/src/js/script.js
@@ -0,0 +1,35 @@
+//$(document).ready(function(){
+// $('.carousel__slider').slick({
+//      speed: 1200,
+//      prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+//      nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+//      responsive: [
+//          {
+//              breakpoint: 992,
+//              settings: {
+//                  dots: true,
+//                  arrows: false
+//              }
+//          }
+//      ]
+//  });});
+$(document).ready(function(){
+    $('.slider').slick({
+        speed: 1200,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/right.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+});
