509520f0fd7f6cca711770f2bc6cb70303f85f92
Destof <webdestof@gmail.com>
2019-08-24 18:36:57 +0500

    adapted

--- Build/dist/js/script.js
+++ Build/dist/js/script.js
@@ -1,3 +1,21 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu_item'),
+    hamburger = document.querySelector('.humburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('humburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('humburger_active');
+            menu.classList.toggle('menu_active');
+        })
+    })
+});
+
 $(document).ready(function(){
     $('.slider').slick({
         speed: 1200,
